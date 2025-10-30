68e0a9dfaa34e12c2e0eddec6851746e7ae11ccd
kse2000 <kse2000@hw.ac.uk>
2025-10-30 13:12:11 +0400

    Changes to Joins SQL code

--- 02_sql/02_joins_groupby.sql
+++ 02_sql/02_joins_groupby.sql
@@ -3,35 +3,35 @@ SELECT
     o.order_id,
     c.name AS customer_name,
     o.order_date,
-    o.total_amount
+    o.status
 FROM orders o
 INNER JOIN customers c
     ON o.customer_id = c.customer_id
 ORDER BY o.order_date DESC
 LIMIT 10;
 
--- 2 LEFT JOIN: All Customers + Orders (if any)
+-- 2  LEFT JOIN: All Customers + Orders (if any)
 SELECT
     c.customer_id,
     c.name,
     o.order_id,
-    o.total_amount
+    o.status
 FROM customers c
 LEFT JOIN orders o
     ON c.customer_id = o.customer_id
 ORDER BY c.customer_id;
 
--- 3 Total Revenue & Average Order Value
+-- 3  Total Revenue & Average Order Value
 SELECT
-    SUM(total_amount) AS total_revenue,
-    AVG(total_amount) AS avg_order_value,
-    COUNT(order_id)   AS total_orders
-FROM orders;
+    SUM(oi.total_price) AS total_revenue,
+    AVG(oi.total_price) AS avg_order_value,
+    COUNT(DISTINCT oi.order_id) AS total_orders
+FROM order_items oi;
 
 -- 4 Category-wise Sales Using JOIN + GROUP BY
 SELECT
     p.category,
-    SUM(oi.quantity * oi.price) AS category_sales
+    SUM(oi.total_price) AS category_sales
 FROM order_items oi
 JOIN products p
     ON oi.product_id = p.product_id
@@ -42,8 +42,10 @@ ORDER BY category_sales DESC;
 WITH customer_revenue AS (
     SELECT
         o.customer_id,
-        SUM(o.total_amount) AS total_spent
-    FROM orders o
+        SUM(oi.total_price) AS total_spent
+    FROM order_items oi
+    JOIN orders o
+        ON oi.order_id = o.order_id
     GROUP BY o.customer_id
 )
 SELECT
